12f0944d5b94029fb1ec1c74c5a03ec0a138dab7
Paul Nitto <pnitto18@gmail.com>
2016-01-28 18:10:09 -0500

    Initial Commit

---
+++ Ski-Places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ski-Places
+//
+//  Created by Paul Nitto on 1/28/16.
+//  Copyright © 2016 Paul Nitto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ski-Places/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ski-Places
+//
+//  Created by Paul Nitto on 1/28/16.
+//  Copyright © 2016 Paul Nitto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
